5b2cf048135f588a8a0b337f375dd32c3df9ad71
“Abdelaziz” <ab.boulmal@gmail.com>
2022-12-03 20:55:37 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,143 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <h1>Todo List</h1>
+    <div>
+      <form class="inputField" @submit.prevent="create">
+        <input v-model="newTodo" autocomplete="off" />
+        <button>Add</button>
+      </form>
+      <ul class="todoList">
+        <li v-for="item in todos" :key="item.id">
+          <h3 :class="{done : item.complete}" @click="complete(item)">{{ item.content }}</h3>
+          <button @click="remove(item)">Delete</button>
+        </li>
+      </ul>
+      <div class="footer">
+        <button @click="markAll">Select All</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const todos = ref([]);
+    const newTodo = ref('');
+    console.log(todos);
+
+    function create() {
+      todos.value.push({
+        id: Date.now(),
+        content: newTodo.value,
+        completed: false,
+      });
+      newTodo.value = '';
+    }
+    function remove(item) {
+      todos.value.splice(todos.value.indexOf(item), 1);
+    }
+    function complete(item){
+      item.complete = !item.complete
+    }
+    function markAll() {
+      todos.value.forEach((todos)=> todos.complete = true)
+    }
+
+    return {
+      todos,
+      newTodo,
+      create,
+      remove,
+      complete,
+      markAll
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  font-family: 'poppins', sans-serif;
+}
+body {
+  width: 100%;
+  height: 100vh;
+  padding: 10px;
+  background: #a9a9a9;
+}
+.wrapper {
+  background-color: #fff;
+  max-width: 400px;
+  width: 100%;
+  margin: 120px auto;
+  padding: 25px;
+  border-radius: 5px;
+  box-shadow: 0px 10px 15px rgb(0, 0, 0, 0.1);
+}
+input {
+  width: 97%;
+  margin: 20px 0;
+  height: 100%;
+  border-radius: 3px;
+  border: 1px solid #ccc;
+  font-size: 17px;
+  padding: 15px 5px;
+  transition: all 0.3s ease;
+}
+form button {
+  width: 100%;
+  height: 100%;
+  border: none;
+  color: #fff;
+  margin-left: 5px;
+  font-size: 21px;
+  outline: none;
+  background: #454548;
+  cursor: pointer;
+  border-radius: 3px;
+  pointer-events: none;
+  transition: all 0.3s ease;
+}
+.wrapper .todoList {
+  margin-top: 20px;
+  max-width: 250px;
+  overflow-y: auto;
+}
+li {
+  position: relative;
+  list-style: none;
+  margin-bottom: 8px;
+  background: #d3d3d3;
+  border-radius: 3px;
+  padding: 10px 15px;
+  cursor: default;
+  overflow: hidden;
+  word-wrap: break-word;
+}
+.footer {
+  display: flex;
+  width: 100%;
+  margin-top: 20px;
+  align-items: center;
+  justify-content: space-between;
+}
+.footer button {
+  padding: 6px 10px;
+  border-radius: 3px;
+  border: none;
+  color: #fff;
+  background: #295538;
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
+}
+.item {
+  cursor: pointer;
 }
 </style>
